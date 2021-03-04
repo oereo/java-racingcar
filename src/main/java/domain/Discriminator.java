a51c4e456ec6c5670692ee0864930cc52418835d
Sehoon In <dlstpgns0406@gmail.com>
2021-03-04 16:42:49 +0900

    feat: 전진 여부 판단 기능 구현

---
+++ src/main/java/domain/Discriminator.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Discriminator {
+    private static final int THRESHOLD = 4;
+
+    public static boolean isMove(int randomNumber) {
+        return (randomNumber >= THRESHOLD);
+    }
+}
