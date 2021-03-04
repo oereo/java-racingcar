69f37d0c27ad62ebc9f6d6202c411a5f7bd01921
Sehoon In <dlstpgns0406@gmail.com>
2021-03-04 17:36:12 +0900

    test: 중복되는 이름의 Car 객체를 추가할 시 NotDulicateNameException를 던지는 test 작성

--- src/test/java/repository/CarManagerTest.java
+++ src/test/java/repository/CarManagerTest.java
@@ -4,13 +4,16 @@ import domain.Car;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarManagerTest {
+
+    private CarManager carManager = new CarManager();
+
     @Test
     void Car_객체를_추가한다() {
         //given
-        CarManager carManager = new CarManager();
         Car car = new Car("oereo");
 
         //when
@@ -19,5 +22,23 @@ class CarManagerTest {
 
         //then
         assertThat(expected).isEqualTo(1);
+    }
+
+    @Test
+    void 중복되는_이름의_Car_객체를_추가를_하면_NotDuplicateNameException을_던진다() {
+        //given
+        Car oereo = new Car("oereo");
+        Car pkalsh = new Car("pkalsh");
+        Car kouz95 = new Car("kouz95");
+        Car pkalsh2 = new Car("pkalsh");
+
+        //when
+        carManager.add(oereo);
+        carManager.add(pkalsh);
+        carManager.add(kouz95);
+
+        //then
+        assertThatExceptionOfType(NotDuplicatedNameException.class)
+            .isThrownBy(carManager.add(pkalsh2));
     }
 }
