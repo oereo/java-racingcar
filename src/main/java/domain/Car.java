29c34ea643469ab8fdf2f2075cf87ede17a5048b
Sehoon In <dlstpgns0406@gmail.com>
2021-03-04 15:47:48 +0900

    feat: 자동차가 움직이는 기능 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -8,5 +8,11 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        position += 1;
+    }
 }
