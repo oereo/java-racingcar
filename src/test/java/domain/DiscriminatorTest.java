a14473df2c29fde3f03b0dd885a57cf46a28740a
Sehoon In <dlstpgns0406@gmail.com>
2021-03-04 16:42:15 +0900

    test: 전진 여부 판단 test code 작성

---
+++ src/test/java/domain/DiscriminatorTest.java
@@ -0,0 +1,33 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscriminatorTest {
+
+    @Test
+    void _4이상이면_전진한다() {
+        //given
+        int generatedNumber = 4;
+
+        //when
+        boolean expected = Discriminator.isMove(generatedNumber);
+
+        //then
+        assertThat(expected).isTrue();
+    }
+
+    @Test
+    void _3이하이면_움직이지_않는다() {
+        //given
+        int generatedNumber = 3;
+
+        //when
+        boolean expected = Discriminator.isMove(generatedNumber);
+
+        //then
+        assertThat(expected).isFalse();
+    }
+}
