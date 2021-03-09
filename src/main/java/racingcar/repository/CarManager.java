c6f3b688c1658ca4d4b08a0b73b6a65da4d201f8
Sehoon In <dlstpgns0406@gmail.com>
2021-03-09 14:52:54 +0900

    feat: 랜덤 숫자 리스트 기준에 따라 플레이어가 이동하는 method 구현

--- src/main/java/racingcar/repository/CarManager.java
+++ src/main/java/racingcar/repository/CarManager.java
@@ -2,6 +2,7 @@ package racingcar.repository;
 
 import racingcar.domain.Car;
 import racingcar.repository.exception.NotDuplicateNameException;
+import racingcar.util.Discriminator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +28,15 @@ public class CarManager {
         for (Car car : carNames) {
             checkDuplicateName(car);
             cars.add(car);
+        }
+    }
+
+    public void moveAllCars(List<Integer> randomNumberList) {
+        for (int i = 0; i < randomNumberList.size(); i++) {
+            Integer randomNumber = randomNumberList.get(i);
+            if (Discriminator.isMove(randomNumber)) {
+                cars.get(i).move();
+            }
         }
     }
 }
