1b7f367ce975af70e0d3d891af3224c855c95e67
Sehoon In <dlstpgns0406@gmail.com>
2021-03-18 22:45:20 +0900

    fix: Car객체 생성 시 5글자 이상의 name을 넣는 테스트 케이스 일괄 수정

--- src/test/java/racingcar/repository/CarManagerTest.java
+++ src/test/java/racingcar/repository/CarManagerTest.java
@@ -23,7 +23,7 @@ class CarManagerTest {
     @Test
     void Car_객체를_추가한다() {
         //given
-        Car car = new Car("oereo");
+        Car car = new Car("oer");
         List<Car> carList = List.of(car);
 
         //when
@@ -37,10 +37,10 @@ class CarManagerTest {
     @Test
     void 중복되는_이름의_Car_객체를_추가를_하면_NotDuplicateNameException을_던진다() {
         //given
-        Car oereo = new Car("oereo");
-        Car pkalsh = new Car("pkalsh");
-        Car kouz95 = new Car("kouz95");
-        Car pkalsh2 = new Car("pkalsh");
+        Car oereo = new Car("oer");
+        Car pkalsh = new Car("pka");
+        Car kouz95 = new Car("kouz");
+        Car pkalsh2 = new Car("pka");
 
         List<Car> carList = List.of(oereo, pkalsh, kouz95, pkalsh2);
 
@@ -53,9 +53,9 @@ class CarManagerTest {
     @Test
     void 랜덤_숫자_리스트를_넘기면_기준에_따라_플레이어가_이동한다() {
         //given
-        Car oereo = new Car("oereo");
-        Car pkalsh = new Car("pkalsh");
-        Car kouz95 = new Car("kouz95");
+        Car oereo = new Car("oer");
+        Car pkalsh = new Car("pka");
+        Car kouz95 = new Car("kouz");
 
         List<Car> carList = List.of(oereo, pkalsh, kouz95);
         carManager.addAllCars(carList);
@@ -77,9 +77,9 @@ class CarManagerTest {
     @Test
     void dto에_존재하는_car_객체가_CarManager의_collection에_존재하는_car_객체와_같다() {
         //given
-        Car oereo = new Car("oereo");
-        Car pkalsh = new Car("pkalsh");
-        Car kouz95 = new Car("kouz95");
+        Car oereo = new Car("oer");
+        Car pkalsh = new Car("pka");
+        Car kouz95 = new Car("kouz");
 
         List<Car> carList = List.of(oereo, pkalsh, kouz95);
         carManager.addAllCars(carList);
@@ -97,9 +97,9 @@ class CarManagerTest {
     @Test
     void 승자가_한명일_때_그_이름을_출력한다() {
         //given
-        Car oereo = new Car("oereo");
-        Car pkalsh = new Car("pkalsh");
-        Car kouz95 = new Car("kouz95");
+        Car oereo = new Car("oer");
+        Car pkalsh = new Car("pka");
+        Car kouz95 = new Car("kouz");
 
         List<Car> carList = List.of(oereo, pkalsh, kouz95);
         carManager.addAllCars(carList);
@@ -109,15 +109,15 @@ class CarManagerTest {
         String winnerMessage = carManager.createWinnerMessage();
 
         //then
-        assertThat(winnerMessage).isEqualTo("pkalsh");
+        assertThat(winnerMessage).isEqualTo("pka");
     }
 
     @Test
     void 승자가_두명이상일_때_그_이름들을_comma로_구분하여_출력한다() {
         //given
-        Car oereo = new Car("oereo");
-        Car pkalsh = new Car("pkalsh");
-        Car kouz95 = new Car("kouz95");
+        Car oereo = new Car("oer");
+        Car pkalsh = new Car("pka");
+        Car kouz95 = new Car("kouz");
 
         List<Car> carList = List.of(oereo, pkalsh, kouz95);
         oereo.move();
@@ -129,7 +129,7 @@ class CarManagerTest {
         String winnerMessage = carManager.createWinnerMessage();
 
         //then
-        assertThat(winnerMessage).isEqualTo("oereo, pkalsh, kouz95");
+        assertThat(winnerMessage).isEqualTo("oer, pka, kouz");
     }
 
 }
