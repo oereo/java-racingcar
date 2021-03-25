d573258e3d010701473b0dc9f2382cb076853021
Sehoon In <dlstpgns0406@gmail.com>
2021-03-25 18:13:31 +0900

    delete: Discriminator class 삭제 및 test case 삭제

--- src/main/java/racingcar/util/Discriminator.java
+++
@@ -1,9 +0,0 @@
-package racingcar.util;
-
-public class Discriminator {
-    private static final int THRESHOLD = 4;
-
-    public static boolean isMove(int randomNumber) {
-        return (randomNumber >= THRESHOLD);
-    }
-}
